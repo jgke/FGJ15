f64b813f7513760798d003c600d832fde71610c1
Juhani Imberg <juhani@imberg.fi>
2015-01-23 21:50:22 +0200

    initial static push

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,15 +3,18 @@ var eventStream = require('event-stream');
 var gulp = require("gulp");
 
 var tsProject = ts.createProject({
-    declarationFiles: true,
-    noExternalResolve: true
+    declarationFiles: false,
+    noExternalResolve: false
 });
 
 gulp.task('scripts', function() {
-    var tsResult = gulp.src('src/*.ts')
-        .pipe(ts(tsProject));
+    var tsResult = gulp.src('src/*.ts').pipe(ts(tsProject));
+    var staticResult = gulp.src("static/*").pipe(gulp.dest("build/"));
+    var vendorPhaser = gulp.src("bower_components/phaser/build/phaser.min.js").pipe(gulp.dest("build/vendor"));
 
     return eventStream.merge( // Merge the two output streams, so this task is finished when the IO of both operations are done.
+        staticResult,
+        vendorPhaser,
         tsResult.dts.pipe(gulp.dest('build/definitions')),
         tsResult.js.pipe(gulp.dest('build/js'))
     );
