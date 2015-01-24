c5c1854d7b9fac268272e22deb67cf582133d396
Jaakko Hannikainen <jaakko.hannikainen@paivola.fi>
2015-01-24 08:59:20 +0200

    continue work on chunks

--- src/Chunk.ts
+++ src/Chunk.ts
@@ -1,3 +1,5 @@
+import GroundTile = require("./GroundTile");
+
 var chunks = [
     [
         [0, 0, 0, 0],
@@ -14,18 +16,15 @@ var chunks = [
 1: solid
  */
 
-class Chunk {
-    data: number[][];
-    offset: number;
-    constructor(offset: number) {
-        this.data = chunks[Math.random() * chunks.length];
-        this.offset = offset;
-    }
-    render() {
-        for(var row in this.data) {
-            for (var tile in row) {
-                if(tile != 0) {
-                    // draw sprite
+export class Chunk extends Phaser.Group {
+    constructor(game: Phaser.Game, offset: number) {
+        super(game);
+        var data = chunks[Math.random() * chunks.length];
+        for(var y = 0; y < data.length; y++) {
+            for(var x = 0; x < data[y].length; x++) {
+                switch(data[y][x]) {
+                    case 1:
+                        this.add(new GroundTile.GroundTile(this.game, offset * 64 + x*64, y * 64));
                 }
             }
         }
